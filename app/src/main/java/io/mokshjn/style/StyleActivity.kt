4ea6e84ac44a4cf7bb6c6489f4e87a8b70c4ac27
Moksh Jain <mokshjn00@gmail.com>
2017-06-29 18:28:19 +0530

    refactor styleactivity

--- app/src/main/java/io/mokshjn/style/StyleActivity.kt
+++ app/src/main/java/io/mokshjn/style/StyleActivity.kt
@@ -3,7 +3,7 @@ package io.mokshjn.style
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class Style : AppCompatActivity() {
+class StyleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
