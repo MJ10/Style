29d23177985c203dcb5d1ece8da0394254309e11
Moksh Jain <mokshjn00@gmail.com>
2017-06-29 18:28:43 +0530

    implement runtime permissions

--- app/src/main/java/io/mokshjn/style/MainActivity.kt
+++ app/src/main/java/io/mokshjn/style/MainActivity.kt
@@ -1,9 +1,11 @@
 package io.mokshjn.style
 
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
-
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import butterknife.bindView
 import com.flurgle.camerakit.CameraListener
 import com.flurgle.camerakit.CameraView
@@ -14,17 +16,35 @@ class MainActivity : AppCompatActivity() {
 
     val camera: CameraView by bindView(R.id.camera)
     val fab: FloatingActionButton by bindView(R.id.fab)
+    val RC_CAMERA = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) + ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) + ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE), RC_CAMERA)
+        }
+
         camera.setCameraListener(object: CameraListener() {
-            override fun onPictureTaken(jpeg: ByteArray?) {
-                startActivity<Style>("image" to jpeg!!)
+            override fun onPictureTaken(jpeg: ByteArray) {
+                startActivity<StyleActivity>("image" to jpeg)
             }
         })
+
         fab.setOnClickListener { camera.captureImage() }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            RC_CAMERA -> {
+                if (grantResults.isNotEmpty()) {
+                    if (grantResults[0] + grantResults[1] + grantResults[2] != PackageManager.PERMISSION_GRANTED) {
+                        finish()
+                    }
+                }
+            }
+        }
     }
 
     override fun onResume() {
